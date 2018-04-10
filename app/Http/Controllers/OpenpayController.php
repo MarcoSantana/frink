e6c36e212c64f67ab4f325eaabbf276d5670a960
Marco A. Santana <marco.santana@gmail.com>
2018-04-10 13:06:56 +0000

    Added base for Openpay.mx

---
+++ app/Http/Controllers/OpenpayController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OpenpayController extends Controller
+{
+    //
+        public function index()
+        {
+               //return "I work" ;
+                return view('openpay.index');
+
+        }
+
+}
